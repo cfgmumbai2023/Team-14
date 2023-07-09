3a24ed6e34994845f2c3fb147355fa74f6916284
rubaltiwari <tiwarirubal0301@gmail.com>
2023-07-09 08:04:27 +0530

    update2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import './App.css';
-import NavBar from './Navbar.js';
-import Academics from './Academics.js';
-import Criteria from './criteria';
 import React, { useState } from "react";
 import Header from "../myapp/components/Header";
 import Footer from "../myapp/components/Footer";
 import Note from "../myapp/components/Note";
 import CreateArea from "../myapp/components/CreateArea";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
-
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -47,64 +36,9 @@ function App() {
           />
         );
       })}
-      <Router>
-    <div className="App">
-      
-      <NavBar/>
-      <Academics/>
-      <Criteria/>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-      <Routes> 
-           <Route exact path="/criteria" element={<Criteria/>}/>
-      </Routes>
-      
-    </div>
-    </Router>
       <Footer />
     </div>
   );
 }
-
-export default App;
-
-
-// import Sidebar from './sidebar';
-// function App() {
-//   return (
-//     <Router>
-//     <div className="App">
-      
-//       <NavBar/>
-//       <Academics/>
-//       <Criteria/>
-//       <Routes> 
-//            <Route exact path="/criteria" element={<Criteria/>}/>
-//       </Routes>
-      
-//     </div>
-//     </Router>
-//   );
-// }
 
 export default App;
