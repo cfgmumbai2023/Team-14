48546dcc87b7d2fd417c5d1c5dca0c87a1110e1b
rubaltiwari <tiwarirubal0301@gmail.com>
2023-07-09 08:05:04 +0530

    update3

---
+++ myapp/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import HighlightIcon from "@material-ui/icons/Highlight";
+import './remark.css'
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        {/* <HighlightIcon /> */}
+        URMI
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
